f34cc856062d4c1e57169becb653d10bbfa92c94
Janna Saine <janna.saine@gmail.com>
2025-10-03 10:56:09 +0300

    Alettiin lisätä Login-toimintoa

--- WebAppFirst/Controllers/HomeController.cs
+++ WebAppFirst/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using System.Data.Entity;  // lisätty itse
 using System.Data.Entity.Infrastructure;  // lisätty itse
 using WebAppFirst.Models;
+using System.Web.UI.WebControls;
 
 
 namespace WebAppFirst.Controllers
@@ -37,6 +38,44 @@ namespace WebAppFirst.Controllers
             ViewBag.Message = "Saapumisohje";
 
             return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ-kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                Session["LoggedStatus"] = "IN";
+                ViewBag.LoginError = 0; //Ei virhettä
+                Session["UserName"] = LoggedUser.UserName;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                Session["LoggedStatus"] = "OUT";
+                ViewBag.LoginError = 1; //Pakotetaan modaali login-ruutu uudelleen, koska kirjautumisyritys epäonnistunut
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                return View("Index", LoginModel);
+                //return View("Login", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
         }
     }
 }
---
+++ WebAppFirst/Models/Logins.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppFirst.Models
+{
+    public class Logins
+    {
+        public int LoginId { get; set; }
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
+        public string LoginErrorMessage { get; internal set; }
+    }
+}
